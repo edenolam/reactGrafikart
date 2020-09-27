0b9526388479182edeb05a425ac3ad9940deeffe
edenolam <julienbasquin.jb@gmail.com>
2020-09-27 16:05:15 +0200

    convertisseur celsius fahrenheit part 1

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+}
 function BoilingVerdict({celsius}) {
     if (celsius >= 100){
         return <div className="alert alert-success">l'eau bout</div>
     }
     return <div className="alert alert-info">l'eau ne bout pas</div>
 }
+class TemperatureInput extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {temperature: ''}
+        this.handleChange = this.handleChange.bind(this)
+    }
 
-ReactDOM.render(<BoilingVerdict celsius={100}/>, document.getElementById('app'))
+    handleChange(e){
+        this.setState({temperature: e.target.value})
+    }
+    render(){
+        const {temperature} = this.state
+        const name = 'scale' + this.props.scale
+        const scaleName = scaleNames[this.props.scale]
+       return <div className="form-group">
+            <label htmlFor={name}>Temperature (en {scaleName})</label>
+            <input type="text" id={name} value={temperature} className="form-control" onChange={this.handleChange}/>
+        </div>
+    }
+}
+class Calculator extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            temperature: ''
+        }
+    }
+
+    render(){
+        const {temperature} = this.state
+        return <div>
+            <TemperatureInput scale="c"/>
+            <TemperatureInput scale="f"/>
+            <BoilingVerdict celsius={parseFloat(temperature)}/>
+        </div>
+    }
+}
+
+ReactDOM.render(<Calculator/>, document.getElementById('app'))
